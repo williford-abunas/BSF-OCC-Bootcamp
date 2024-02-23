be2832f2f5cc36bad73e458c51d8ed8cddafb207
williford-abunas <abunaswilliford@gmail.com>
2024-02-23 18:58:27 +1300

    JS exercise

---
+++ Day-3-JS-Intro/index.js
@@ -0,0 +1,9 @@
+const shirt = 17
+const shoes = 65
+const pants = 49
+const sunglasses = 13
+
+let total = 2 * shirt + shoes + pants + 3 * sunglasses
+const discount = total * 0.1
+
+console.log(`Total: $ ${total - discount}`)
